e49270fa5cad09f449de18c89f02fb67ea28c58b
Kedrick06 <67480509+Kedrick06@users.noreply.github.com>
2020-08-26 18:12:32 -0500

    Add files via upload

---
+++ Parrot.java
@@ -0,0 +1,18 @@
+package Practice3.demo;
+
+public class Parrot {
+ static boolean getUp (boolean sqwak, int currentHour) {
+ 
+ if (currentHour < 6) {
+	 sqwak = true;
+ }
+ else if(currentHour > 22) {
+	 sqwak = true;
+ }
+ 	return sqwak;
+}
+
+public static void main (String[] args) {
+	System.out.println(getUp(true, 2));
+}
+}
